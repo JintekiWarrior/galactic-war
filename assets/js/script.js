c6f2a812cacd751174878e0663c7e7d5b919f291
Moe Yassine <moeyassine1993@gmail.com>
2022-03-16 16:24:14 -0400

    Add gameboard with a 10 X 10 spaces

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-const root = $("#root")
+const appRoot = $("#root")
+
+// gameboard will be a 10 X 10 board with 100 spaces 
+let spaceId = 1 
+
+for (let row = 1; row <= 10; row++) {
+  const rowElement = $("<div>")
+  rowElement.attr('id', `row-${row}`)
+  appRoot.append(rowElement)
+  for (let col = 1; col <= 10; col++) {
+    const space = $("<div>")
+    space.attr('id', `space-${spaceId}`)
+    space.addClass("space-element")
+    space.text("test")
+    rowElement.append(space)
+    spaceId++
+  }
+}
