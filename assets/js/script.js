da5a8ce094152beae58a14a6728f0d9cb3f49a97
Moe Yassine <moeyassine1993@gmail.com>
2022-03-17 15:04:38 -0400

    Add css to center the gameboard - Try to add img to a space in the gameboard however ran into a bug.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,35 @@
 const appRoot = $("#root")
 
-// gameboard will be a 10 X 10 board with 100 spaces 
-let spaceId = 1 
+/* function that creates the gameboard */
+const gameBoard = function() {
+  let spaceId = 1 
 
-for (let row = 1; row <= 10; row++) {
-  const rowElement = $("<div>")
-  rowElement.attr('id', `row-${row}`)
-  appRoot.append(rowElement)
-  for (let col = 1; col <= 10; col++) {
-    const space = $("<div>")
-    space.attr('id', `space-${spaceId}`)
-    space.addClass("space-element")
-    space.text("test")
-    rowElement.append(space)
-    spaceId++
+  for (let row = 1; row <= 10; row++) {
+  
+    const rowElement = $("<div>")
+    rowElement.attr('id', `row-${row}`)
+    appRoot.append(rowElement)
+  
+    for (let col = 1; col <= 10; col++) {
+  
+      const space = $("<div>")
+      space.attr('id', `space-${spaceId}`)
+      space.addClass("space-element")
+      space.text("test")
+  
+      rowElement.append(space)
+  
+      spaceId++
+    }
   }
 }
+
+gameBoard()
+
+const start = $("#space-95")
+const charOne = $("<img />", {
+  src: 'images/knight.jpg'
+})
+start.append(charOne)
+
+
